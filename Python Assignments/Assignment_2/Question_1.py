b6683fa44773a6162edacc91daae56134a4f286a
Srinivas Mathur <51886178+srinivasmathur@users.noreply.github.com>
2021-04-29 22:01:17 +0530

    Add files via upload

---
+++ Python Assignments/Assignment_2/Question_1.py
@@ -0,0 +1,9 @@
+for line in range(5):
+  for n in range(line):
+    print('*',end=' ')
+  print('')
+
+for line in range(5,0,-1):
+  for n in range(line):
+    print('*',end=' ')
+  print('')
---
+++ Python Assignments/Assignment_2/Question_2.py
@@ -0,0 +1,2 @@
+word = input('Enter a word: ')
+print(f"Reversed word : {word[::-1]}")
